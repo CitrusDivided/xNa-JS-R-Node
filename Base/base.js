7c910ca9e2a01c6fb2d87ed0359546518540d20d
CitrusDivided <78776670+CitrusDivided@users.noreply.github.com>
2023-01-03 14:01:42 -0800

    Further imp. of canvas - audio, video, platform

--- Base/base.js
+++ Base/base.js
@@ -1,6 +1,13 @@
+// Preconnect - canvas
 var canvas = document.querySelector('canvas');
 
-canvas.width = 800;
-canvas.height = 480;
+canvas.width = 1280;
+canvas.height = 720;
 
 var c = canvas.getContext('2d');
+
+//Laying out foundation for audio/video feed
+c.fillStyle = "rgb(0,0,0,0.9)";
+c.font = '12px arial';
+c.textAlign = 'left';
+c.fillText('Testing - audio, video platform 0.1a', 25, 35);
